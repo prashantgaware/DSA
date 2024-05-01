23d4bd1cd236c52eed2e9ca3d39ccb2b88b38bb0
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-01 19:57:47 +0530

    237. Delete Node in a Linked List

---
+++ LinkedList/src/leetcode/Test1.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+/**
+ * 237. Delete Node in a Linked List
+ *
+ * There is a singly-linked list head and we want to delete a node node in it.
+ *
+ * You are given the node to be deleted node. You will not be given access to the first node of head.
+ *
+ * All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+ *
+ * Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+ *
+ * The value of the given node should not exist in the linked list.
+ * The number of nodes in the linked list should decrease by one.
+ * All the values before node should be in the same order.
+ * All the values after node should be in the same order.
+ * Custom testing:
+ *
+ * For the input, you should provide the entire linked list head and the node to be given node. node should not be the last node of the list and should be an actual node in the list.
+ * We will build the linked list and pass the node to your function.
+ * The output will be the entire list after calling your function.
+ */
+
+class Node {
+    int val;
+    Node next;
+    Node(int val){
+        this.val = val;
+    }
+}
+
+public class Test1 {
+
+    public static void deleteNode(Node node){
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    private static void printLinkedList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.next = new Node(5);
+        head.next.next = new Node(1);
+        head.next.next.next = new Node(9);
+
+        Node nodeToDelete = head.next;
+
+        System.out.println("Original linked list:");
+        printLinkedList(head);
+
+        deleteNode(nodeToDelete);
+
+        System.out.println("\nLinked list after deleting node:");
+        printLinkedList(head);
+    }
+}
