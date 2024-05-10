eb78cd3c61a2b6f7ea4b27c334e33a0b87a982af
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-10 21:33:46 +0530

    LearningDSA : Recursion

---
+++ LearningDSA/src/recursion/Multiply.java
@@ -0,0 +1,27 @@
+package recursion;
+
+// Write a program to find n*m (n & m are positive integers) using recursion.
+
+import java.util.Scanner;
+
+public class Multiply {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter M : ");
+        int m = scanner.nextInt();
+        System.out.print("Enter N : ");
+        int n = scanner.nextInt();
+
+        int mul = mul(m, n);
+        System.out.println(mul);
+
+    }
+
+    static int mul(int m, int n) {
+        if (n == 0)
+            return 0;
+        else
+            return m + mul(m, n-1);
+    }
+}
