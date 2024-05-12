d09e1c9c67ece89b61aaba88805c3df4681e8579
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-12 12:27:41 +0530

    Searching : BinarySearchIterative, BinarySearchRecursive

---
+++ Searching/src/learning/binary/BinarySearchIterative.java
@@ -0,0 +1,39 @@
+package learning.binary;
+
+import java.util.Scanner;
+
+public class BinarySearchIterative {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter Array Elements :");
+        for (int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter target : ");
+        int target = scanner.nextInt();
+
+        System.out.println(bSearchIterative(arr, n, target));
+    }
+
+    private static int bSearchIterative(int[] arr, int n, int target) {
+        int low = 0, high = n-1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (target > arr[mid]) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ Searching/src/learning/binary/BinarySearchRecursive.java
@@ -0,0 +1,39 @@
+package learning.binary;
+
+import java.util.Scanner;
+
+public class BinarySearchRecursive {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        System.out.print("Enter Array Elements :");
+        for (int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        int low = 0, high = n-1;
+
+        System.out.print("Enter target : ");
+        int target = scanner.nextInt();
+
+        System.out.println(bSearchRecursive(arr, low, high, target));
+    }
+
+    private static int bSearchRecursive(int[] arr, int low, int high, int target) {
+        if(low > high)
+            return -1;
+
+        int mid = (low+high) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+        else if (target > arr[mid])
+            return bSearchRecursive(arr, mid+1, high, target);
+        else
+            return bSearchRecursive(arr, low, mid-1, target);
+    }
+
+}
