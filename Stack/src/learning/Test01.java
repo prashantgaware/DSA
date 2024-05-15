752d4da7600498b2a77ab8ed651ba8faee531a54
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-15 22:55:12 +0530

    Stack

---
+++ Stack/src/learning/Test01.java
@@ -0,0 +1,20 @@
+package learning;
+
+import java.util.Scanner;
+
+public class Test01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of array element : ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements : ");
+        for (int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+
+
+    }
+}
