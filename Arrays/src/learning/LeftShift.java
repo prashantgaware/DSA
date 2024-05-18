fa85fcbb4aff1f96eaa2001206cc99a1c4be2064
Prashant Gaware <prashantgaware8@gmail.com>
2024-05-18 14:58:37 +0530

    LeftShift

---
+++ Arrays/src/learning/LeftShift.java
@@ -0,0 +1,31 @@
+package learning;
+
+import java.util.Arrays;
+
+public class LeftShift {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {1,2,3,4,5};
+        int d = 3;
+        System.out.println(Arrays.toString(leftShiftByOne(arr1)));
+        System.out.println(Arrays.toString(leftShiftByD(arr2, d)));
+    }
+
+    private static int[] leftShiftByOne(int[] arr) {
+        int temp = arr[0];
+        for (int i=0; i<arr.length-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        arr[arr.length - 1] = temp;
+
+        return arr;
+    }
+
+    private static int[] leftShiftByD(int[] arr, int d) {
+        for (int i=0; i<d; i++) {
+            leftShiftByOne(arr);
+        }
+
+        return arr;
+    }
+}
--- Arrays/src/leetcode/IsSortedAndRotated.java
+++ Arrays/src/leetcode/IsSortedAndRotated.java
@@ -1,38 +1,39 @@
 package leetcode;
 
-// 1752. Check if Array Is Sorted and Rotated
 /*
-Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
+    1752. Check if Array Is Sorted and Rotated
 
-There may be duplicates in the original array.
+    Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
 
-Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+    There may be duplicates in the original array.
 
-
-
-Example 1:
-
-Input: nums = [3,4,5,1,2]
-Output: true
-Explanation: [1,2,3,4,5] is the original sorted array.
-You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
-Example 2:
-
-Input: nums = [2,1,3,4]
-Output: false
-Explanation: There is no sorted array once rotated that can make nums.
-Example 3:
-
-Input: nums = [1,2,3]
-Output: true
-Explanation: [1,2,3] is the original sorted array.
-You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+    Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
 
 
-Constraints:
 
-1 <= nums.length <= 100
-1 <= nums[i] <= 100
+    Example 1:
+
+    Input: nums = [3,4,5,1,2]
+    Output: true
+    Explanation: [1,2,3,4,5] is the original sorted array.
+    You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
+    Example 2:
+
+    Input: nums = [2,1,3,4]
+    Output: false
+    Explanation: There is no sorted array once rotated that can make nums.
+    Example 3:
+
+    Input: nums = [1,2,3]
+    Output: true
+    Explanation: [1,2,3] is the original sorted array.
+    You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+
+
+    Constraints:
+
+    1 <= nums.length <= 100
+    1 <= nums[i] <= 100
  */
 
 public class IsSortedAndRotated {
@@ -54,10 +55,10 @@ public class IsSortedAndRotated {
 
     public static void main(String[] args) {
         IsSortedAndRotated sol = new IsSortedAndRotated();
-        System.out.println(sol.check(new int[]{3, 4, 5, 1, 2})); // true
-        System.out.println(sol.check(new int[]{2, 1, 3, 4}));     // false
-        System.out.println(sol.check(new int[]{1, 2, 3}));       // true
-        System.out.println(sol.check(new int[]{1, 1, 1}));       // true
-        System.out.println(sol.check(new int[]{2, 2, 2, 2, 3, 4, 2, 2})); // false
+        System.out.println(sol.check(new int[]{3, 4, 5, 1, 2}));
+        System.out.println(sol.check(new int[]{2, 1, 3, 4}));
+        System.out.println(sol.check(new int[]{1, 2, 3}));
+        System.out.println(sol.check(new int[]{1, 1, 1}));
+        System.out.println(sol.check(new int[]{2, 2, 2, 2, 3, 4, 2, 2}));
     }
 }
