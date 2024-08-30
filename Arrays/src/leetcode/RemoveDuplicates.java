bac784e99af7ead094fc6d29c5aedd305107e886
Prashant Gaware <prashantgaware8@gmail.com>
2024-08-30 23:03:38 +0530

    Removing Duplicates

--- Arrays/src/leetcode/RemoveDuplicates.java
+++ Arrays/src/leetcode/RemoveDuplicates.java
@@ -63,8 +63,10 @@ public class RemoveDuplicates {
         for (int i = 0; i < k; i++) {
             System.out.print(nums[i] + " ");
         }
+        System.out.println();
     }
 
+    // Will work when array is sorted.
     private static int removeDuplicatesEff(int[] nums) {
         if (nums.length == 0)
             return 0;
