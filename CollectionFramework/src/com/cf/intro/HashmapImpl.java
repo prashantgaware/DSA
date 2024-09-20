0f27bde1d38f86c81a1505730d15715ffb5fd0b6
Prashant Gaware <prashantgaware8@gmail.com>
2024-09-20 23:43:03 +0530

    Hashmap custom impl

---
+++ CollectionFramework/src/com/cf/intro/HashmapImpl.java
@@ -0,0 +1,10 @@
+package com.cf.intro;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashmapImpl {
+    public static void main(String[] args) {
+        Set<String> hs = new HashSet<>();
+    }
+}
