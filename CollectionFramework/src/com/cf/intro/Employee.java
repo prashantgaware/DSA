57d27cf6553c241a962d2cbf36980e47eb40caf6
Prashant Gaware <prashantgaware8@gmail.com>
2024-09-21 23:13:50 +0530

    Hashmap custom impl : put() internal implementation

--- CollectionFramework/src/com/cf/intro/Employee.java
+++ CollectionFramework/src/com/cf/intro/Employee.java
@@ -2,4 +2,40 @@ package com.cf.intro;
 
 public class Employee {
 
+    private int id;
+
+    public Employee() { }
+
+    public Employee(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Employee e  = (Employee) obj;
+        if (id == e.id)
+            return true;
+        else
+            return false;
+    }
 }
--- CollectionFramework/src/com/cf/intro/HashmapImpl.java
+++ CollectionFramework/src/com/cf/intro/HashmapImpl.java
@@ -1,10 +1,28 @@
 package com.cf.intro;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HashmapImpl {
     public static void main(String[] args) {
-        Set<String> hs = new HashSet<>();
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 55);
+        map.put("A", 56);
+        map.put("A", 57);
+        map.put("A", 58);
+        System.out.println(map.size());
+        System.out.println(map);
+        System.out.println();
+
+        Employee e1 = new Employee(121);
+        Employee e2 = new Employee(121);
+        System.out.println(e1.hashCode());
+        System.out.println(e2.hashCode());
+
+        Map<Employee, String> empMap = new HashMap<>();
+        empMap.put(e1, "Prashant");
+        empMap.put(e2, "Saurav");
+        System.out.println(empMap.size());
+        System.out.println(empMap);
     }
 }
