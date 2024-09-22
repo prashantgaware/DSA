3776f4c6e80d143f38b10291f9cbcd08d89d7d30
Prashant Gaware <prashantgaware8@gmail.com>
2024-09-22 19:40:32 +0530

    HashSet custom impl : add() implementation

---
+++ CollectionFramework/src/com/cf/intro/HashSetInternals.java
@@ -0,0 +1,32 @@
+package com.cf.intro;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetInternals {
+    public static void main(String[] args) {
+        Set<String> hs = new HashSet<>();
+        hs.add("A");            // map.put("A",PRESENT)
+        hs.add("B");            // map.put("B",PRESENT)
+        hs.add("C");            // map.put("C",PRESENT)
+        hs.add("D");            // map.put("D",PRESENT)
+        hs.add("E");            // map.put("E",PRESENT)
+        System.out.println(hs.size());
+        hs.add("A");
+        System.out.println(hs.size());
+
+        Set<Employee> empSet = new HashSet<>();
+        Employee e1 = new Employee(123);
+        Employee e2 = new Employee(124);
+        Employee e3 = new Employee(124);
+        Employee[] employees = {e1, e2, e3};
+
+        for (Employee employee : employees)
+            if (empSet.add(employee)) {
+
+            } else
+                System.out.println("Duplicate : " + employee);
+
+        System.out.println(empSet.size());
+    }
+}
