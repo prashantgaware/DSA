b44d5559680d856c440a0017798fd4d84110b9c3
Prashant Gaware <prashantgaware8@gmail.com>
2024-11-17 13:37:52 +0530

    DSA-GFG-160Days : Reverse array

--- Arrays/src/fundamentals/MissingNumber.java
+++ Arrays/src/fundamentals/MissingNumber.java
@@ -5,6 +5,8 @@ public class MissingNumber {
         int[] arr = {1, 2, 3, 4, 5, 6, 8, 9, 10};
         int missingNumber = findMissingNumber(arr);
         System.out.println("The missing number is: " + missingNumber);
+        int missingNumberUsingSum = findMissingNumberUsingSum(arr);
+        System.out.println("The missing number using sum is: " + missingNumberUsingSum);
     }
 
     private static int findMissingNumber(int[] arr) {
@@ -21,5 +23,21 @@ public class MissingNumber {
         }
 
         return -1;
+    }
+
+    /**
+     * This method finds the missing number in an array of n-1 elements
+     * @param arr array of n-1 elements
+     * @return missing number
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
+    private static int findMissingNumberUsingSum(int[] arr) {
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum - arr[i];
+        }
+        return sum;
     }
 }
---
+++ DSA-GFG-160Days/src/D_003_ReverseArray.java
@@ -0,0 +1,26 @@
+public class D_003_ReverseArray {
+    /**
+     * Reverse the array
+     * @param arr - array to be reversed
+     *            Time Complexity: O(n)
+     *            Space Complexity: O(1)
+     */
+    private static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        for (int i : arr)
+            System.out.print(i + " ");
+    }
+}
