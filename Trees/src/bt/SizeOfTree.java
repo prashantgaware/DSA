f9b366ac3e789bb6de889417d6f3b993d80fa348
Prashant Gaware <prashantgaware8@gmail.com>
2024-12-05 23:49:13 +0530

    DSA Tree: sizeUsingLevelOrderTraversal

--- Trees/src/bt/SizeOfTree.java
+++ Trees/src/bt/SizeOfTree.java
@@ -1,5 +1,8 @@
 package bt;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class SizeOfTree {
     private static int size(TreeNode root) {
         if (root != null) {
@@ -7,5 +10,38 @@ public class SizeOfTree {
         }
 
         return 0;
+    }
+
+    private static int sizeUsingLevelOrderTraversal(TreeNode root) {
+        int count = 0;
+        if (root == null) return count;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode temp = q.poll();
+            count++;
+            if (temp.left != null)
+                q.offer(temp.left);
+            if (temp.right != null)
+                q.offer(temp.right);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(6);
+        root.left.right.left = new TreeNode(5);
+        root.left.right.right = new TreeNode(11);
+        root.right.right = new TreeNode(9);
+        root.right.right.left = new TreeNode(4);
+
+        System.out.println("Size of tree: " + sizeUsingLevelOrderTraversal(root));
     }
 }
