2f6c5e83e14d38e65ede7804633dd2676bed86d0
Prashant Gaware <prashantgaware8@gmail.com>
2024-12-06 23:22:37 +0530

    DSA Stack: Stack Reversal

---
+++ Stack/src/learning/ReverseStack.java
@@ -0,0 +1,31 @@
+package learning;
+
+import java.util.Stack;
+
+public class ReverseStack {
+    private static void stackReversal(Stack<Integer> stack) {
+        int size = stack.size();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = stack.pop();
+        }
+
+        for (int i  = 0; i < size; i++)
+            stack.push(array[i]);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        // Push some elements into the stack
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println("Original Stack: " + stack);
+        stackReversal(stack);
+        System.out.println("Reversed Stack: " + stack);
+    }
+}
