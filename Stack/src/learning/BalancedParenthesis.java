c73cda558a5b7780c15c7d512290acaa06974088
Prashant Gaware <prashantgaware8@gmail.com>
2024-12-06 22:55:17 +0530

    DSA Stack: Valid Parenthesis

--- Stack/src/learning/BalancedParenthesis.java
+++ Stack/src/learning/BalancedParenthesis.java
@@ -46,8 +46,39 @@ public class BalancedParenthesis {
         return stack.isEmpty();
     }
 
+    private static boolean isValidParenthesis(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s== null || s.length() <= 0)
+            return true;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else
+                    return false;
+            } else if (s.charAt(i) == '}') {
+                if (!stack.isEmpty() && stack.peek() == '{')
+                    stack.pop();
+                else
+                    return false;
+            } else if (s.charAt(i) == ']') {
+                if (!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else
+                    return false;
+            } else {
+                stack.push(s.charAt(i));
+            }
+        }
+
+        if (stack.isEmpty())
+            return true;
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println(isBalancedParenthesis("(({}[[()]]))"));
         System.out.println(isBalancedParenthesis2("(({}[[(]]))"));
+        System.out.println(isValidParenthesis("(({}[[()]]))"));
     }
 }
