207d2a807b5c188d3cb1452b808571697afa3743
Prashant Gaware <prashantgaware8@gmail.com>
2024-12-06 23:09:31 +0530

    DSA Stack: PalindromeValidationWithSpecialMiddleCharacterX

---
+++ Stack/src/learning/PalindromeValidationWithSpecialMiddleCharacterX.java
@@ -0,0 +1,33 @@
+package learning;
+
+public class PalindromeValidationWithSpecialMiddleCharacterX {
+    private static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) == 'x') {
+                i++;
+                continue;
+            }
+
+            if (str.charAt(j) == 'x') {
+                j--;
+                continue;
+            }
+
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String string = "ababaxababa";
+        System.out.println(isPalindrome(string));
+    }
+}
