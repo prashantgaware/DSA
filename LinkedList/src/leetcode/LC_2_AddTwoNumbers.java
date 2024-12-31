dcca22cc2d23bb2f157ce235b693b3f97283af4a
Prashant Gaware <prashantgaware8@gmail.com>
2024-12-31 23:45:37 +0530

    LL: 2. Add Two Numbers

---
+++ LinkedList/src/leetcode/LC_2_AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class LC_2_AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+
+        if (carry > 0)
+            current.next = new ListNode(carry);
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        LC_2_AddTwoNumbers atn = new LC_2_AddTwoNumbers();
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = atn.addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.print(result.val + " -> ");
+            result = result.next;
+        }
+        System.out.println("null");
+    }
+}
