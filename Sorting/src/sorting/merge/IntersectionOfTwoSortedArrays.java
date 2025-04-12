ad0811ace314c7685cc13da89f7bf6d4f340e01e
Prashant Gaware <prashantgaware8@gmail.com>
2025-04-12 19:37:49 +0530

    IntersectionOfTwoSortedArrays

--- Sorting/src/sorting/merge/IntersectionOfTwoSortedArrays.java
+++ Sorting/src/sorting/merge/IntersectionOfTwoSortedArrays.java
@@ -2,10 +2,26 @@ package sorting.merge;
 
 public class IntersectionOfTwoSortedArrays {
     public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 3, 5, 6, 7};
 
+        System.out.print("Intersection of two sorted arrays: ");
+        intersection(arr1, arr2);
     }
 
-    private static void intersection() {
-
+    private static void intersection(int[] arr1, int[] arr2) {
+        int i = 0;
+        int j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] == arr2[j]) {
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+            } else if (arr1[i] < arr2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
     }
 }
