31552c4a20b7befa8386a4bc43469c330a78f474
Prashant Gaware <prashantgaware8@gmail.com>
2025-05-10 14:11:08 +0530

    LL : Add two numbers

--- LinkedList/src/learning/AddTwoNumbers.java
+++ LinkedList/src/learning/AddTwoNumbers.java
@@ -14,6 +14,8 @@ public class AddTwoNumbers {
         Node result = addTwoNumber(l1, l2);
         printList(result); // Output: 7 -> 0 -> 8
 
+        Node res = addTwoNums(l1, l2);
+        printList(res); // Output: 7 -> 0 -> 7
     }
 
     private static void printList(Node head) {
@@ -23,6 +25,35 @@ public class AddTwoNumbers {
             current = current.next;
         }
         System.out.println();
+    }
+
+    public static Node addTwoNums(Node h1, Node h2) {
+        Node dummy = new Node(-1);
+        Node curr = dummy;
+        int carry = 0;
+
+        while (h1 != null || h2 != null) {
+            int sum = carry;
+            if (h1 != null)
+                sum += h1.data;
+            if (h2 != null)
+                sum += h2.data;
+
+            carry = sum / 10;
+            curr.next = new Node(sum % 10);
+            curr = curr.next;
+
+            if (h1 != null)
+                h1 = h1.next;
+            if ((h2 != null))
+                h2 = h2.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new Node(carry);
+            curr = curr.next;
+        }
+        return dummy.next;
     }
 
     public static Node addTwoNumber(Node l1, Node l2) {
