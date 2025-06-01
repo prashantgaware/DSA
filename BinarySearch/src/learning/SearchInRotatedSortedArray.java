5e3fee2d68ec2a0d5e6cc8590f057afbf8f9f7b4
Prashant Gaware <prashantgaware8@gmail.com>
2025-06-01 23:06:37 +0530

    Search In Rotated Sorted Array

---
+++ BinarySearch/src/learning/SearchInRotatedSortedArray.java
@@ -0,0 +1,39 @@
+package learning;
+
+public class SearchInRotatedSortedArray {
+    private static int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (arr[left] <= arr[mid]) {
+                if (arr[left] <= target && target < arr[mid]) {
+                    right = mid - 1; // Target is in the left half
+                } else {
+                    left = mid + 1; // Target is in the right half
+                }
+            } else { // Right half is sorted
+                if (arr[mid] < target && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(arr, target)); // Output: 4
+    }
+}
---
+++ BinarySearch/src/learning/SearchInRotatedSortedArrayWithDuplicates.java
@@ -0,0 +1,44 @@
+package learning;
+
+public class SearchInRotatedSortedArrayWithDuplicates {
+    public static int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // this condition handles the case where we have duplicates
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            } else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,4,5,6,7,0,1,2,4};
+        int target = 0;
+        System.out.println("found at: " + search(nums, target));
+    }
+}
