c28c630136e92df0005f24475ee9ce3492598394
Prashant Gaware <prashantgaware8@gmail.com>
2025-06-14 10:24:32 +0530

    Insert in BT recursive

--- Trees/src/bt/InsertInBT.java
+++ Trees/src/bt/InsertInBT.java
@@ -1,7 +1,5 @@
 package bt;
 
-import com.sun.source.tree.Tree;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -19,8 +17,15 @@ public class InsertInBT {
 
         insertInBT(root, 10);
         printTree(root);
+        System.out.println();
+        insertInBTIterative(root, 12);
+        printTree(root);
+        System.out.println();
+        insertInBTRecursive(root, 15);
+        printTree(root);
     }
 
+    // This code is valid only for BST, not for BT.
     private static TreeNode insertInBT(TreeNode root, int k) {
         if (root == null)
             return new TreeNode(k);
@@ -75,6 +80,34 @@ public class InsertInBT {
                 temp.right = new TreeNode(k);
                 return root;
             }
+        }
+
+        return root;
+    }
+
+    /**
+     * Insert a node in Binary Tree recursively.
+     * @param root The root of the binary tree.
+     * @param k The value to be inserted.
+     * @return The root of the binary tree after insertion.
+     * Approach:
+     * 1. If the root is null, create a new node with value k and return it.
+     * 2. If the left child is null, insert the new node as the left child.
+     * 3. If the right child is null, insert the new node as the right child.
+     * 4. If both children are present, recursively call the function on the left child.
+     */
+    public static TreeNode insertInBTRecursive(TreeNode root, int k) {
+        if (root == null) {
+            return new TreeNode(k);
+        }
+
+        if (root.left == null) {
+            root.left = new TreeNode(k);
+        } else if (root.right == null) {
+            root.right = new TreeNode(k);
+        } else {
+            // If both children are present, insert in the left subtree
+            insertInBTRecursive(root.left, k);
         }
 
         return root;
