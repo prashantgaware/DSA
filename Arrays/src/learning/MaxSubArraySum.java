63f3f5af429b177559cacd303e98c8fed278e3ea
Prashant Gaware <prashantgaware8@gmail.com>
2025-08-27 23:00:29 +0530

    max sub array sum

--- Arrays/src/learning/MaxSubArraySum.java
+++ Arrays/src/learning/MaxSubArraySum.java
@@ -1,12 +1,26 @@
 package learning;
 
 public class MaxSubArraySum {
+    /**
+     *
+     * @param arr
+     * @return
+     * Approach: Kadane's Algorithm
+     * 1. Initialize maxSum to the smallest integer value and currentSum to 0
+     * 2. Iterate through each element in the array
+     * 3. Add the current element to currentSum
+     * 4. If currentSum is greater than maxSum, update maxSum
+     * 5. If currentSum becomes negative, reset it to 0
+     * 6. Return maxSum
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     */
     public static int maxSubArraySum(int[] arr) {
         int maxSum = Integer.MIN_VALUE;
         int currentSum = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            currentSum += arr[i];
+        for (int i : arr) {
+            currentSum += i;
             if (currentSum > maxSum) {
                 maxSum = currentSum;
             }
@@ -19,6 +33,15 @@ public class MaxSubArraySum {
     }
 
     // Kadane's Algorithm
+    /*
+    1. Initialize two variables, maxSofar and maxEndingHere, to the first element of the array.
+    2. Iterate through the array starting from the second element.
+    3. For each element, update maxEndingHere to be the maximum of the current element and the sum of maxEndingHere and the current element.
+    4. Update maxSofar to be the maximum of maxSofar and maxEndingHere.
+    5. After iterating through the array, return maxSofar as the result.
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+    * */
     static int maxSubarraySum(int[] arr) {
         int maxSofar = arr[0];
         int maxEndingHere = arr[0];
