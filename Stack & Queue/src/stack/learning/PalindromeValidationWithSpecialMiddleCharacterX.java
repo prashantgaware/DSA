442dc48d38a3641cf218430843445b8a029558de
Prashant Gaware <prashantgaware8@gmail.com>
2025-09-07 15:20:10 +0530

    palindrome validation

--- Stack & Queue/src/stack/learning/PalindromeValidationWithSpecialMiddleCharacterX.java
+++ Stack & Queue/src/stack/learning/PalindromeValidationWithSpecialMiddleCharacterX.java
@@ -1,5 +1,15 @@
 package stack.learning;
 
+// https://www.geeksforgeeks.org/check-if-a-given-string-is-palindrome-or-not-when-one-character-can-be-removed/
+
+/**
+ * Palindrome Validation With Special Middle Character 'x'
+ * Given a string, check if it can be converted to a palindrome by removing at most one character.
+ * The string may contain the special character 'x', which can be ignored when checking for palindrome.
+ * For example, the string "abaxab" can be converted to a palindrome by removing the character 'x'.
+ * The string "abaxabx" can also be converted to a palindrome by ignoring the 'x' characters.
+ */
+
 public class PalindromeValidationWithSpecialMiddleCharacterX {
     private static boolean isPalindrome(String str) {
         int i = 0;
