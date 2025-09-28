87aca35d54eccc2171e11661c5ab51e13bf05a90
Prashant Gaware <prashantgaware8@gmail.com>
2025-09-28 13:11:37 +0530

    search in rotated sorted array

--- BinarySearch/src/learning/SearchInRotatedSortedArray.java
+++ BinarySearch/src/learning/SearchInRotatedSortedArray.java
@@ -1,6 +1,27 @@
 package learning;
 
 public class SearchInRotatedSortedArray {
+    /**
+     * Search in a rotated sorted array
+     * @param arr - rotated sorted array
+     * @param target - target value to search
+     * @return index of target if found, otherwise -1
+     * Approach:
+     * 1. Initialize two pointers, left and right, to the start and end of
+     *   the array.
+     * 2. While left is less than or equal to right, calculate the mid index.
+     * 3. If the middle element is the target, return the mid index.
+     * 4. Determine which half of the array is sorted.
+     * 5. If the left half is sorted, check if the target lies within this
+     *   half. If it does, adjust the right pointer to mid - 1. Otherwise,
+     *   adjust the left pointer to mid + 1.
+     * 6. If the right half is sorted, check if the target lies within this
+     *   half. If it does, adjust the left pointer to mid + 1. Otherwise,
+     *   adjust the right pointer to mid - 1.
+     * 7. If the target is not found, return -1.
+     * Time Complexity: O(log n)
+     * Space Complexity: O(1)
+     */
     private static int search(int[] arr, int target) {
         int left = 0;
         int right = arr.length - 1;
