6806782f14fd81dd5df024d4584a50b1fffc0cdf
Prashant Gaware <prashantgaware8@gmail.com>
2025-09-29 22:56:31 +0530

    search in rotated sorted array 2 duplicate elements

---
+++ BinarySearch/src/leetcode/LC_81_SearchInRotatedSortedArray2.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+public class LC_81_SearchInRotatedSortedArray2 {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,2,3,3,0,0,1};
+        System.out.println(search(arr, 0));
+    }
+
+    public static boolean search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                return true;
+            }
+
+            //skipping duplicates
+            if (arr[left] == arr[mid] && arr[mid] == arr[right]) {
+                left++; right--;
+                continue;
+            }
+
+            if (arr[left] <= arr[mid]) {
+                if (target >= arr[left] && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target > arr[mid] && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
