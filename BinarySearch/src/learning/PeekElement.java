2401f603fe62cc20722e5116c8acf0eff05ddaf1
Prashant Gaware <prashantgaware8@gmail.com>
2025-09-30 23:36:16 +0530

    peak element

--- BinarySearch/src/learning/PeekElement.java
+++ BinarySearch/src/learning/PeekElement.java
@@ -32,7 +32,7 @@ public class PeekElement {
     public static int findPeak(int[] arr) {
         int low = 0;
         int high = arr.length-1;
-        while (low <= high) {
+        while (low < high) {
             int mid = low + (high-low) / 2;
             if (arr[mid] < arr[mid+1]) {
                 low = mid + 1;
