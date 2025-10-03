ea7e8dc51efab6b7482b4aeefe89acfacccdc11b
Prashant Gaware <prashantgaware8@gmail.com>
2025-10-03 23:10:08 +0530

    lc 100 same tree

---
+++ Trees/src/leetcode/LC_100_SameTree.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+public class LC_100_SameTree {
+    public static boolean isSame(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return (p.val == q.val) && isSame(p.left, q.left) && isSame(p.left, p.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+        System.out.println(isSame(p, q));
+    }
+}
